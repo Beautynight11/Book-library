53d714efd95be4c7920b365ac09ead4fae2f306c
mariaezhova <m.petelina@yandex.ru>
2022-11-16 23:30:15 +0100

    Created new vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+   <button @click="getData">CLICK ME</button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      error: 'Error',
+      data: null,
+      errored: false,
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
